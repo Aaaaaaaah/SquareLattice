276bf8a32403b95a4232f5f24707060ed2af9f32
ldh <panda12345pa@163.com>
2017-10-27 18:06:58 +0800

    修改了SVD部分，尚未debug

--- mps.py
+++ mps.py
@@ -6,14 +6,15 @@ from node import Node
 A = Node(["l","r","phy"],[5,3,2])
 B = Node(["l","r","phy"],[3,5,2])
 
-Node.connect(A,"l",B,"r")
-Node.connect(A,"r",B,"l")
+#Node.connect(A,"l",B,"r")
+#Node.connect(A,"r",B,"l")
 
 ep = 0.1
 
 H = np.reshape([[0.25,0,0,0],[0,-0.25,0.5,0],[0,0.5,-0.25,0],[0,0,0,0.25]],[2,2,2,2])
 I = np.reshape(np.identity(4),[2,2,2,2])
-expH = I - 4.*ep*H
+expH = Node(["lowerLeft","lowerRight","upperRight","upperLeft"],[2,2,2,2])
+expH.data = I - 4.*ep*H
 
 for _ in range(10):
     Node.update(A,"l",B,"r",expH)
--- node.py
+++ node.py
@@ -38,6 +38,14 @@ class Node:
         tags = T1.tags + T2.tags
         order1 = [T1.tags.index(i) for i in tags1]
         order2 = [T2.tags.index(i) for i in tags2]
+        for i in tags1:
+            j = T1.tags.index(i)
+            temp = np.ones(T1.dll,dtype=np.int)
+            t1.data *= np.reshape(T1.env[j],temp)
+        for i in tags2:
+            j = T2.tags.index(i)
+            temp = np.ones(T2.dll,dtype=np.int)
+            t2.data *= np.reshape(T2.env[j],temp)
         for i in tags1 + tags2:
             temp = tags.index(i)
             tags = tags[:temp] + tags[temp+1:]
@@ -61,34 +69,25 @@ class Node:
 
     @staticmethod
     def update(T1,tag1,T2,tag2,H):
-        ##1 TempT = contract(T1,tag1,T2,Tag2)
-        ##2 TempT = contract(TempT,"phy1",H,"lowerLeft")
-        ##3 TempT = contract(TempT,"phy2",H,"lowerRight")
         ##  SVD
-        #1 乘 Env
+        #0 初始缩并
         i1 = T1.find_leg_index(tag1)
         i2 = T2.find_leg_index(tag2)
-        assert T1.dl[i1] == T2.dl[i2]
-        TD1 = T1.data.copy()
-        TD2 = T2.data.copy()
-        for i,j in enumerate(T1.env):
-            tmp = np.ones(T1.dll+1,dtype=np.int)
-            tmp[i] = T1.dl[i]
-            TD1 *= np.reshape(j*j,tmp)
-        for i,j in enumerate(T2.env):
-            tmp = np.ones(T2.dll+1,dtype=np.int)
-            tmp[i] = T2.dl[i]
-            TD2 *= np.reshape(j*j,tmp)
-        #2 两个Tensor相乘
-        TD = np.tensordot(TD1,TD2,[[i1],[i2]])
-        #3 乘上Hamiltonian
-        TD = np.tensordot(TD,H,[[T1.dll-1,-1],[0,1]])
-        tmp = list(range(T1.dll-1))+[T1.dll+T2.dll-2]+list(range(T1.dll-1,T1.dll+T2.dll-2))+[T1.dll+T2.dll-1]
-        TD = np.transpose(TD,tmp)
-        #4 SVD
+        TempT = contract(T1,[tag1],T2,[Tag2])
+        TempT = contract(TempT,"phy",H,"lowerLeft")
+        TempT = contract(TempT,"phy'",H,"lowerRight")
+        #1 乘 Env
+        TD = TempT.data.copy()
+        for i,j in enumerate(TempT.env):
+            tmp = np.ones(TempT.dll,dtype=np.int)
+            tmp[i] = TempT.dl[i]
+            TD *= np.reshape(j*j,tmp)
+        #2 SVD
+        temp = list(range(T1.dll)) + [TempT.dll-2] + list(range(T1.dll,TempT.dll-2)) + [TempT.dll-1]
+        TD = np.transpose(TD,temp)
         sh = TD.shape
-        sh1 = list(sh[:T1.dll])
-        sh2 = list(sh[T1.dll:])
+        sh1 = list(sh[:T1.dll-1])
+        sh2 = list(sh[T1.dll-1:])
         TD = np.reshape(TD,[reduce(int.__mul__,sh1),reduce(int.__mul__,sh2)])
         U,S,V = np.linalg.svd(TD)
         T1.env[i1]=np.sqrt(S[:T1.dl[i1]])
@@ -99,24 +98,22 @@ class Node:
         sh2 = [T2.dl[i2]] + sh2
         U = np.reshape(U[:,:T1.dl[i1]],sh1)
         V = np.reshape(V[:T2.dl[i2],:],sh2)
-        o1 = list(range(i1)) + [-1] + list(range(i1,T1.dll))
-        o2 = list(range(1,i2+1)) + [0] + list(range(i2+1,T2.dll+1))
+        o1 = list(range(i1)) + [-1] + list(range(i1,T1.dll-1))
+        o2 = list(range(1,i2+1)) + [0] + list(range(i2+1,T2.dll))
         T1.data = np.transpose(U,o1)
         T2.data = np.transpose(V,o2)
-        #5 吐Env
+        #3 吐Env
         for i,j in enumerate(T1.env):
             if i == i1:
                 continue
-            tmp = np.ones(T1.dll+1,dtype=np.int)
+            tmp = np.ones(T1.dll,dtype=np.int)
             tmp[i] = T1.dl[i]
-            TD1 /= np.reshape(j*j,tmp)
+            T1.data /= np.reshape(j*j,tmp)
         for i,j in enumerate(T2.env):
             if i == i2:
                 continue
-            tmp = np.ones(T2.dll+1,dtype=np.int)
+            tmp = np.ones(T2.dll,dtype=np.int)
             tmp[i] = T2.dl[i]
-            TD2 /= np.reshape(j*j,tmp)
+            T2.data /= np.reshape(j*j,tmp)
         T1.data/=np.max(np.abs(T1.data))
         T2.data/=np.max(np.abs(T2.data))
-        print(T1.env[0])
-        print(T1.env[1])
