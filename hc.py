e055a2363e233a9ae80ff2c26f1c7276f2fceb30
Hao Zhang <zh970205@outlook.com>
2017-11-09 02:49:41 +0800

    hc

---
+++ hc.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from node import Node
+
+L1 = 5
+L2 = 6
+D = 4
+
+#   u
+# l   r
+#   d
+
+# +-+ +-+ +-+
+# | | | | | |
+# + +-+ +-+ +
+# | | | | | |
+# +-+ +-+ +-+
+# | | | | | |
+# + +-+ +-+ +
+# | | | | | |
+# +-+ +-+ +-+
+
+def _node(s):
+    l = [D]*len(s)
+    l.append(2)
+    return Node(list(s)+["p"],l)
+
+def node_in_lattice(i,j):
+    if (i-j)%2 == 0:
+        s = "udr"
+    else:
+        s = "udl"
+    if i == 0:
+        s = s.replace("u","")
+    if i == L1-1:
+        s = s.replace("d","")
+    if j == 0:
+        s = s.replace("l","")
+    if j == L2-1:
+        s = s.replace("r","")
+    return _node(s)
+
+lattice = [[node_in_lattice(i,j) for j in range(L2)] for i in range(L1)]
+
+ep = 0.1
+
+H = np.reshape([[0.25,0,0,0],[0,-0.25,0.5,0],[0,0.5,-0.25,0],[0,0,0,0.25]],[2,2,2,2])
+I = np.reshape(np.identity(4),[2,2,2,2])
+expH = lambda x:I - x*ep*H
+
+for t in range(10):
+    print t
+    for i in range(0,L1):
+        for j in range(i%2,L2-1,2):
+            Node.update(lattice[i][j],lattice[i][j+1],"r","l","p","p",expH(1))
+    for j in range(0,L2):
+        for i in range(j%2,L1-1,2):
+            Node.update(lattice[i][j],lattice[i+1][j],"d","u","p","p",expH(2))
+    for j in range(0,L2):
+        for i in range((j+1)%2,L1-1,2):
+            Node.update(lattice[i][j],lattice[i+1][j],"d","u","p","p",expH(3))
+
+print lattice[1][1].envs
--- sl.py
+++ sl.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# 4 x 4 OBD
 
 import numpy as np
 from node import Node
@@ -48,7 +47,7 @@ H = np.reshape([[0.25,0,0,0],[0,-0.25,0.5,0],[0,0.5,-0.25,0],[0,0,0,0.25]],[2,2,
 I = np.reshape(np.identity(4),[2,2,2,2])
 expH = I - 4.*ep*H
 
-for t in range(100):
+for t in range(10):
     print t
     for i in range(0,L1):
         for j in range(0,L2-1,2):
