672a77c0194fd622ab6347dcaab689693cecbdbc
Hao Zhang <zh970205@outlook.com>
2017-11-09 04:25:58 +0800

    port-to-tf

--- node.py
+++ node.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import tensorflow as tf
 
 class Node:
 
@@ -8,22 +9,22 @@ class Node:
         assert len(tags) == len(dims)
         assert len(set(tags)) == len(tags)
         if data is not None:
-            self.data = np.reshape(np.array(data,dtype=np.float64),dims)
-            self.data /= np.max(np.abs(self.data))
+            self.data = tf.reshape(tf.cast(data,tf.float32),dims)
+            self.data /= tf.reduce_max(tf.abs(self.data))
         else:
-            self.data = np.random.random(dims)
+            self.data = tf.random_uniform(dims)
         if envs is not None:
             self.envs = []
             for i,j in zip(dims,envs):
                 if j is None:
-                    self.envs.append(np.ones(i))
+                    self.envs.append(tf.ones(i))
                 else:
-                    tmp = np.array(j,dtype=np.float64)
-                    tmp /= np.max(np.abs(tmp))
+                    tmp = tf.cast(j,tf.float32)
+                    tmp /= tf.reduce_max(tf.abs(tmp))
                     assert tmp.shape == (i,)
                     self.envs.append(tmp)
         else:
-            self.envs = [np.ones(i) for i in dims]
+            self.envs = [tf.ones(i) for i in dims]
         self.dims = list(dims)
         self.tags = list(tags)
 
@@ -33,37 +34,29 @@ class Node:
         self.dims = other.dims
         self.tags = other.tags
 
-    @staticmethod
-    def copy(self):
-        return Node(self.tags,self.dims,self.data,self.envs)
-    @staticmethod
-    def connect(T1,tag1,T2,tag2):
-        T1.envs[T1.tags.index(tag1)] = T2.envs[T2.tags.index(tag2)]
-
     def rename_leg(self,tag_dict):
         for i,j in tag_dict.items():
             self.tags[self.tags.index(i)] = j
 
     @staticmethod
     def absorb_envs(self,pow,legs=None):
-        ans = self.data.copy()
+        ans = self.data
         if legs == None:
             legs = range(len(self.dims))
         for i in legs:
             tmp = np.ones(len(self.dims),dtype=int)
             tmp[i] = self.dims[i]
-            ans *= np.reshape(np.power(self.envs[i],pow),tmp)
+            temp = tf.reshape(self.envs[i],tmp)
+            if pow > 0:
+                for _ in range(pow):
+                    ans *= temp
+            else:
+                for _ in range(-pow):
+                    ans /= temp
         return ans
 
-    @staticmethod
     def transpose(self,tags):
-        data = np.transpose(self.data,[self.tags.index(i) for i in tags])
-        dims = [self.dims[self.tags.index(i)] for i in tags]
-        envs = [self.envs[self.tags.index(i)] for i in tags]
-        return Node(tags,dims,data,envs)
-
-    def transpose(self,tags):
-        self.data = np.transpose(self.data,[self.tags.index(i) for i in tags])
+        self.data = tf.transpose(self.data,[self.tags.index(i) for i in tags])
         tmp = self.dims
         self.dims = [tmp[self.tags.index(i)] for i in tags]
         tmp = self.envs
@@ -91,27 +84,27 @@ class Node:
         dims = [j for i,j in enumerate(T1.dims) if i not in order1] + [j for i,j in enumerate(T2.dims) if i not in order2]
         envs = [j for i,j in enumerate(T1.envs) if i not in order1] + [j for i,j in enumerate(T2.envs) if i not in order2]
         #initiate the answer
-        T = Node(tags,dims,np.tensordot(TD1,TD2,[order1,order2]),envs)
+        T = Node(tags,dims,tf.tensordot(TD1,TD2,[order1,order2]),envs)
         return T
 
     @staticmethod
     def svd(self,num,tag1,tag2,cut):
         dims1 = self.dims[:num]
         dims2 = self.dims[num:]
-        data1, env, data2 = np.linalg.svd(
-            np.reshape(
+        env, data1, data2 = tf.svd(
+            tf.reshape(
                 Node.absorb_envs(self,2),
                 [np.prod(dims1),np.prod(dims2)])
         )
-        env = np.sqrt(env[:cut])
+        env = tf.sqrt(env[:cut])
         data1 = data1[:,:cut]
-        data2 = data2[:cut,:]
+        data2 = data2[:,:cut]
         tags1 = self.tags[:num] + [tag1]
-        tags2 = [tag2] + self.tags[num:]
+        tags2 = self.tags[num:] + [tag2]
         dims1 = dims1 + [cut]
-        dims2 = [cut] + dims2
+        dims2 = dims2 + [cut]
         envs1 = self.envs[:num] + [env]
-        envs2 = [env] + self.envs[num:]
+        envs2 = self.envs[num:] + [env]
         T1,T2 = Node(tags1,dims1,data1,envs1),Node(tags2,dims2,data2,envs2)
         T1.data = Node.absorb_envs(T1,-2,range(len(dims1)-1))
         T2.data = Node.absorb_envs(T2,-2,range(1,len(dims2)))
--- sl.py
+++ sl.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import tensorflow as tf
+from tensorflow.python.client import timeline
 from node import Node
 
 L1 = 4
@@ -44,10 +46,10 @@ lattice = [[node_in_lattice(i,j) for j in range(L2)] for i in range(L1)]
 ep = 0.1
 
 H = np.reshape([[0.25,0,0,0],[0,-0.25,0.5,0],[0,0.5,-0.25,0],[0,0,0,0.25]],[2,2,2,2])
-I = np.reshape(np.identity(4),[2,2,2,2])
+I = np.reshape(np.identity(4,dtype=np.float32),[2,2,2,2])
 expH = I - 4.*ep*H
 
-for t in range(10):
+for t in range(1):
     print t
     for i in range(0,L1):
         for j in range(0,L2-1,2):
@@ -62,4 +64,21 @@ for t in range(10):
         for i in range(1,L1-1,2):
             Node.update(lattice[i][j],lattice[i+1][j],"d","u","p","p",expH)
 
-print lattice[1][1].envs
+config = tf.ConfigProto()
+#config.device_count["GPU"] = 0
+#config.gpu_options.allow_growth = True
+#config.graph_options.optimizer_options.global_jit_level=tf.OptimizerOptions.ON_1
+config.log_device_placement = True
+sess = tf.Session(config=config)
+sess.run(tf.global_variables_initializer())
+
+options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+run_metadata = tf.RunMetadata()
+
+sess = tf.Session()
+print sess.run(lattice[1][1].envs[0],options=options, run_metadata=run_metadata)
+
+fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+chrome_trace = fetched_timeline.generate_chrome_trace_format()
+with open('timeline.json', 'w') as f:
+    f.write(chrome_trace)
