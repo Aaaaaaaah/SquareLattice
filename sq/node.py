219d2e28338cf5efc38bb5fb9da7681a85bcc634
Hao Zhang <zh970205@outlook.com>
2018-03-04 23:19:46 +0800

    node重构完毕

--- sq/node.py
+++ sq/node.py
@@ -38,6 +38,18 @@ class Node(object):
                     raise Exception("cannot init node data") from e
             else:
                 self.data = np.random.random(self.dims)
+        else:
+            self.data = None
+
+    @classmethod
+    def copy_shape(cls, tensor):
+        return cls(tensor.tags, tensor.dims, data=None, init_data=False)
+
+    def reshape(self, tags, dims=None):
+        self.dims = self.check_dims(dims)
+        self.tags = self.check_dims(tags, len(self.dims))
+        if self.data:
+            self.data = np.reshape(self.data, self.dims)
 
     def __repr__(self):
         return "Node with dims: %s"%str(zip(self.tags, self.dims))
@@ -104,18 +116,27 @@ class Node(object):
         result.data = data
         return result
 
-    @staticmethod
-    def svd(tensor, tags, tag1, tag2, cut=None):
-        if type(tags) != int:
+    @classmethod
+    def svd(cls, tensor, tags, tag1, tag2, cut=None):
+        assert isinstance(tag1, str), "tag should be str"
+        assert isinstance(tag2, str), "tag should be str"
+        assert tag1 not in tensor.tags, "duplicated tags"
+        assert tag2 not in tensor.tags, "duplicated tags"
+
+        if isinstance(tags, list):
             for i in tags:
-                assert i in tensor.tags
-            assert len(tags) == len(set(tags))
+                assert i in tensor.tags, "invalid tags"
+            assert len(tags) == len(set(tags)), "duplicated tags"
+
             num = len(tags)
             tags = tags + list(set(tensor.tags) - set(tags))
-            tensor_transposed = Node.transpose(tensor, tags)
-        else:
+            tensor_transposed = cls.transpose(tensor, tags)
+        elif isinstance(tags, int):
             num = tags
             tensor_transposed = tensor
+        else:
+            raise Exception("unrecognized tags format")
+
         dims1 = tensor_transposed.dims[:num]
         dims2 = tensor_transposed.dims[num:]
         data1, env, data2 = np.linalg.svd(
@@ -123,35 +144,52 @@ class Node(object):
                 tensor_transposed.data,
                 [np.prod(dims1), np.prod(dims2)])
         )
+
         if not cut:
             cut = len(env)
+        else:
+            assert isinstance(cut, int), "cut dim should be int"
+            assert cut <= len(env), "cut dim should be less than total dim"
+
         env = env[:cut]
+
         data1 = data1[:, :cut]
         tags1 = tensor_transposed.tags[:num] + [tag1]
         dims1 = dims1 + [cut]
         data1 = np.reshape(data1, dims1)
+
         data2 = data2[:cut, :]
         tags2 = [tag2] + tensor_transposed.tags[num:]
         dims2 = [cut] + dims2
         data2 = np.reshape(data2, dims2)
-        tensor1 = Node(tags1, dims1, None, False)
-        tensor1.data = data1
-        tensor2 = Node(tags2, dims2, None, False)
-        tensor2.data = data2
+
+        tensor1 = cls(tags1, dims1, None, False)
+        tensor1.data = np.reshape(data1, dims1)
+        tensor2 = cls(tags2, dims2, None, False)
+        tensor2.data = np.reshape(data2, dims2)
         return tensor1, env, tensor2
 
-    @staticmethod
-    def qr(tensor, tags, tag1, tag2, cut=None):
-        if type(tags) != int:
+    @classmethod
+    def qr(cls, tensor, tags, tag1, tag2, cut=None):
+        assert isinstance(tag1, str), "tag should be str"
+        assert isinstance(tag2, str), "tag should be str"
+        assert tag1 not in tensor.tags, "duplicated tags"
+        assert tag2 not in tensor.tags, "duplicated tags"
+
+        if isinstance(tags, list):
             for i in tags:
-                assert i in tensor.tags
-            assert len(tags) == len(set(tags))
+                assert i in tensor.tags, "invalid tags"
+            assert len(tags) == len(set(tags)), "duplicated tags"
+
             num = len(tags)
             tags = tags + list(set(tensor.tags) - set(tags))
-            tensor_transposed = Node.transpose(tensor, tags)
-        else:
+            tensor_transposed = cls.transpose(tensor, tags)
+        elif isinstance(tags, int):
             num = tags
             tensor_transposed = tensor
+        else:
+            raise Exception("unrecognized tags format")
+
         dims1 = tensor_transposed.dims[:num]
         dims2 = tensor_transposed.dims[num:]
         data1, data2 = np.linalg.qr(
@@ -159,18 +197,25 @@ class Node(object):
                 tensor_transposed.data,
                 [np.prod(dims1), np.prod(dims2)])
         )
+
         if not cut:
-            cut = len(data2)
+            cut = data1.shape[1]
+        else:
+            assert isinstance(cut, int), "cut dim should be int"
+            assert cut <= data1.shape[1], "cut dim should be less than total dim"
+
         data1 = data1[:, :cut]
         tags1 = tensor_transposed.tags[:num] + [tag1]
         dims1 = dims1 + [cut]
         data1 = np.reshape(data1, dims1)
+
         data2 = data2[:cut, :]
         tags2 = [tag2] + tensor_transposed.tags[num:]
         dims2 = [cut] + dims2
         data2 = np.reshape(data2, dims2)
-        tensor1 = Node(tags1, dims1, None, False)
-        tensor1.data = data1
-        tensor2 = Node(tags2, dims2, None, False)
-        tensor2.data = data2
+
+        tensor1 = cls(tags1, dims1, None, False)
+        tensor1.data = np.reshape(data1, dims1)
+        tensor2 = cls(tags2, dims2, None, False)
+        tensor2.data = np.reshape(data2, dims2)
         return tensor1, tensor2
