0e5a007b824cdb38a1cc179c5ab88f834d9bcfed
Hao Zhang <zh970205@outlook.com>
2018-03-04 22:50:36 +0800

    还有svd和qr没加assert

--- sq/node.py
+++ sq/node.py
@@ -1,68 +1,106 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import tensorflow as tf
 
 class Node(object):
 
-    # 初始化函数
+    @staticmethod
+    def check_dims(dims):
+        try:
+            tmp_dims = list(dims)
+        except Exception:
+            raise Exception("dims cannot be converted to list")
+        for i in tmp_dims:
+            assert isinstance(i, int), 'dim should be int'
+        return tmp_dims
+
+    @staticmethod
+    def check_tags(tags, length):
+        try:
+            tmp_tags = list(tags)
+        except Exception:
+            raise Exception("tags cannot be converted to list")
+        for i in tmp_tags:
+            assert isinstance(i, str), 'tag should be str'
+        assert len(set(tmp_tags)) == len(tmp_tags), 'tags should not duplicated'
+        assert len(tmp_tags) == length, 'invalid length of tags'
+        return tmp_tags
+
     def __init__(self, tags, dims, data=None, init_data=True):
-        assert len(tags) == len(dims) # tags 和 dims 长度需要一样
-        assert len(set(tags)) == len(tags) # tags没有 重复
-        self.dims = list(dims)
-        self.tags = list(tags)
+        self.dims = self.check_dims(dims)
+        self.tags = self.check_dims(tags, len(self.dims))
 
         if init_data:
             if data:
-                self.data = np.reshape(np.array(data, np.float32), self.dims)
+                try:
+                    self.data = np.reshape(np.array(data, np.float32), self.dims)
+                except Exception as e:
+                    raise Exception("cannot init node data") from e
             else:
                 self.data = np.random.random(self.dims)
 
     def __repr__(self):
         return "Node with dims: %s"%str(zip(self.tags, self.dims))
 
-    #重命名脚,吸收环境等基本操作
-    def rename_leg(self, tag_dict):
-        tmp = [self.tags.index(i) for i in tag_dict]
-        for i in tmp:
-            self.tags[i] = tag_dict[self.tags[i]]
-        return self
+    def rename_leg(self, new_tags):
+        if isinstance(new_tags, list):
+            self.tags = self.check_tags(new_tags, len(self.tags))
 
-    #运算
+        elif isinstance(new_tags, dict):
+            for i, j in new_tags.items():
+                try:
+                    self.tags[self.tags.index(i)] = j
+                except Exception as e:
+                    raise Exception("error when replace tag") from e
+
+        else:
+            raise Exception("new_tags must be list or dict")
 
     #转置
-    @staticmethod
-    def transpose(tensor, tags):
+    @classmethod
+    def transpose(cls, tensor, tags):
+        tags = cls.check_tags(tags, len(tensor.tags))
         data = np.transpose(tensor.data, [tensor.tags.index(i) for i in tags])
         dims = [tensor.dims[tensor.tags.index(i)] for i in tags]
-        result = Node(tags, dims, None, False)
+        result = cls(tags, dims, None, False)
         result.data = data
         return result
 
     #张量操作
-    @staticmethod
-    def contract(tensor1, tags1, tensor2, tags2, tags_dict1=None, tags_dict2=None):
+    @classmethod
+    def contract(cls, tensor1, tags1, tensor2, tags2, tags_dict1=None, tags_dict2=None):
         if tags_dict1 is None:
             tags_dict1 = {}
+        else:
+            assert isinstance(tags_dict1, dict)
         if tags_dict2 is None:
             tags_dict2 = {}
+        else:
+            assert isinstance(tags_dict2, dict)
+
         # order:the indexs of legs waiting for contracting
-        order1 = [tensor1.tags.index(i) for i in tags1]
-        order2 = [tensor2.tags.index(i) for i in tags2]
-        # generate the contribute of the answer
+        try:
+            order1 = [tensor1.tags.index(i) for i in tags1]
+            order2 = [tensor2.tags.index(i) for i in tags2]
+        except Exception as e:
+            raise Exception("tag to contract not match") from e
+
         for i in tags_dict1:
-            assert i in tensor1.tags and i not in tags1
+            assert i in tensor1.tags and i not in tags1, "tag to change not match"
         for i in tags_dict2:
-            assert i in tensor2.tags and i not in tags2
+            assert i in tensor2.tags and i not in tags2, "tag to change not match"
+
+        # generate the contribute of the answer
         tags = [j if j not in tags_dict1 else tags_dict1[j] \
                 for i, j in enumerate(tensor1.tags) if i not in order1] +\
                [j if j not in tags_dict2 else tags_dict2[j] \
                 for i, j in enumerate(tensor2.tags) if i not in order2]
         dims = [j for i, j in enumerate(tensor1.dims) if i not in order1] +\
                 [j for i, j in enumerate(tensor2.dims) if i not in order2]
+
         #initiate the answer
         data = np.tensordot(tensor1.data, tensor2.data, [order1, order2])
-        result = Node(tags, dims, None, False)
+        result = cls(tags, dims, None, False)
         result.data = data
         return result
 
